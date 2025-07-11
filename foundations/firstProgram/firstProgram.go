af8f1b22b66db21df58e2bb0651e9ad6705acbad
Myke <myke.amorim@usp.br>
2025-07-11 20:45:00 -0300

    feat: first program in Golang

---
+++ foundations/firstProgram/firstProgram.go
@@ -0,0 +1,11 @@
+package main // Executable programs starts by "main" package
+
+import "fmt"
+
+/*
+* Main function is the entry point for Go programs.
+ */
+func main() {
+	fmt.Println("Hello")
+	fmt.Print("World!")
+}
